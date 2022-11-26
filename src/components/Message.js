4c4c47f13ae47aa8009fb2e156dbe200255079c2
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-26 03:25:53 -0800

    change type

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,11 +3,10 @@ import React, { useEffect, useState } from 'react';
 function Message({ size, featherCount }) {
   const [message, setMessage] = useState('');
   useEffect(() => {
-    console.log(featherCount);
     const msg =
-      featherCount > 0
-        ? `There are ${featherCount} feathers`
-        : 'Oh my! Your bird is naked!';
+      Number(featherCount) === 0
+        ? 'Oh my! Your bird is naked!'
+        : `There are ${featherCount} feathers`;
     console.log('Message', size);
     console.log('🚀 ~ file: Message.js ~ line 7 ~ useEffect ~ msg', msg);
     setMessage(msg);
