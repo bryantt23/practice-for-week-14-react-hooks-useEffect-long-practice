cf3649ef2f7a812337b4f1239656d69a2c16d480
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-26 03:24:59 -0800

    check for zero featherse

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Message({ size }) {
+function Message({ size, featherCount }) {
+  const [message, setMessage] = useState('');
   useEffect(() => {
+    console.log(featherCount);
+    const msg =
+      featherCount > 0
+        ? `There are ${featherCount} feathers`
+        : 'Oh my! Your bird is naked!';
     console.log('Message', size);
-  }, [size]);
-  return <div className='message medium'>(Oh my! Your bird is naked!)</div>;
+    console.log('🚀 ~ file: Message.js ~ line 7 ~ useEffect ~ msg', msg);
+    setMessage(msg);
+  }, [size, featherCount]);
+  return <div className='message medium'>{message}</div>;
 }
 
 export default Message;
